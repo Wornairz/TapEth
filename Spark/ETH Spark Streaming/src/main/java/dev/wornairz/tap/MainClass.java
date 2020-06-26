9cd8998e09cd849ed044bb0b17d54b1097f4b99d
Wornairz <wornairz@users.noreply.github.com>
2020-06-26 23:45:26 +0200

    Added ElasticSearch and Kibana integration

--- Spark/ETH Spark Streaming/src/main/java/dev/wornairz/tap/MainClass.java
+++ Spark/ETH Spark Streaming/src/main/java/dev/wornairz/tap/MainClass.java
@@ -1,6 +1,8 @@
 package dev.wornairz.tap;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
@@ -13,7 +15,6 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -23,7 +24,6 @@ import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.regression.LinearRegression;
 import org.apache.spark.ml.regression.LinearRegressionModel;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
@@ -42,7 +42,7 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
-
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -57,14 +57,15 @@ public class MainClass {
 	public static void main(String[] args) throws Exception {
 		SparkConf sparkConf = new SparkConf().setAppName("Ethereum Spark").setMaster("local[2]");
 		sparkConf.set("es.index.auto.create", "true");
+		sparkConf.set("es.nodes", "10.0.100.51");
+		sparkConf.set("es.resource", "tap/eth");
+		sparkConf.set("es.input.json", "yes");
+	    //sparkConf.set("es.nodes.wan.only", "true");
+	    
 		spark = SparkSession.builder().config(sparkConf).getOrCreate();
 		spark.sparkContext().setLogLevel("WARN");
 		
-		HttpClient client = HttpClient.newHttpClient();
-		String prediction = client.send(HttpRequest.newBuilder(new URI(
-				"https://ethgasstation.info/api/predictTable.json?api-key=0dd9fc3a3da130f1ad75664d09f4f849b7638819615c7d6cca229a7c943c"))
-				.build(), BodyHandlers.ofString()).body();
-		JSONArray predictionJson = new JSONArray(prediction);
+		JSONArray predictionJson = getPredictionJson();
 
 		List<Row> data = new ArrayList<>();
 		for (int i = 0; i < predictionJson.length(); i++) {
@@ -100,19 +101,28 @@ public class MainClass {
 		// spark.stop();
 	}
 
+	private static JSONArray getPredictionJson() throws IOException, InterruptedException, URISyntaxException {
+		HttpClient client = HttpClient.newHttpClient();
+		String prediction = client.send(HttpRequest.newBuilder(new URI(
+				"https://ethgasstation.info/api/predictTable.json?api-key=0dd9fc3a3da130f1ad75664d09f4f849b7638819615c7d6cca229a7c943c"))
+				.build(), BodyHandlers.ofString()).body();
+		JSONArray predictionJson = new JSONArray(prediction);
+		return predictionJson;
+	}
+
 	private static void convertRDDtoDataset(JavaRDD<String> rdd) {
 		JavaRDD<Row> rowRDD = rdd.map(s -> RowFactory.create(s));
 		Dataset<Row> dataset = spark.read().json(rdd);
 		if (!dataset.isEmpty()) {
 			dataset = dataset.drop("blockHash", "transactionIndex", "nonce", "input", "r", "s", "v", "blockNumber",
 					"gas", "from", "to", "value", "hash");
-			dataset.show();
-			dataset = dataset.map((MapFunction<Row, Row>) MainClass::convertGasPriceToDouble, RowEncoder.apply(new StructType(new StructField[]{new StructField("gasPrice", DataTypes.DoubleType, false, Metadata.empty())})));
-			dataset = dataset.withColumn("gasPrice", conv(col("gasPrice"), 16, 10).cast(DataTypes.DoubleType));
+			dataset = dataset.map((MapFunction<Row, Row>) MainClass::convertGasPriceToDouble, RowEncoder.apply(new StructType(new StructField[]{new StructField("gasPrice", DataTypes.DoubleType, false, Metadata.empty())})))
+				.withColumn("gasPrice", conv(col("gasPrice"), 16, 10).cast(DataTypes.DoubleType));
 			dataset = new VectorAssembler().setInputCols(new String[] { "gasPrice" }).setOutputCol("features")
 					.transform(dataset).withColumn("label", lit(1d).cast(DataTypes.DoubleType));
-			dataset.show();
-			lrModel.transform(dataset).show();		
+			Dataset<Row> predictionDataset = lrModel.transform(dataset);
+			predictionDataset.show();
+			JavaEsSpark.saveJsonToEs(predictionDataset.toJSON().toJavaRDD(), "tap/eth");
 		}
 	}
 	
